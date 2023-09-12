7bfed44d2df808a3de16f3b2f5ed7d89725229bd
sdas1 <sdas1@miraclesoft.com>
2023-09-12 23:04:53 +0530

    todo-list

--- todolist/src/Todo.js
+++ todolist/src/Todo.js
@@ -2,6 +2,26 @@ import React, { useEffect, useState } from 'react';
 import './Project.css';
 
 function Todo(){
+    const [setTodo,setName]=useState('');
+    const [listitem,sublist]=useState([]);
+
+    function addbutton(){
+        // sublist([...listitem,setTodo])
+        // setTodo('')
+        // console.log(listitem);
+        sublist((listitem)=>{
+            const updateList=[...listitem,setTodo]
+            console.log(updateList);
+            setName('');
+            return updateList
+        })
+    }
+    function removeActivity(i){
+        const updateList=listitem.filter((element,id)=>{
+            return i!=id;
+        })
+        sublist(updateList)
+    }
 return(
     <>
       <div className='container' >
@@ -9,7 +29,30 @@ return(
             <h2>Todo list</h2>
 
         </div>
-        
+        <input type='text' value={setTodo} onChange={(e)=>setName(e.target.value)}></input>
+        <button onClick={addbutton}>add</button>
+        <p className='List-heading'>Here is Your List : {")"}</p>
+        {
+            listitem!=[] && listitem.map((data,i)=>{
+                return(
+                    <>
+                    
+                    <p key={i}>
+                        <div className='listData'>{data}
+
+                        </div>
+                            <div className='btn-position'>
+                                <button className='green-button' onClick={() => removeActivity(i)}>
+                                    Remove
+                                </button>
+                            </div>
+                    </p>
+                   
+                    </>
+                )
+            })
+        }
+
     </div>
   </>
 )
